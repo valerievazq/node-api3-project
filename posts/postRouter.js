6f749740add81c8b8d86e1a9414e3ee2dd660669
Valerie <53797814+valerievazq@users.noreply.github.com>
2020-12-09 17:21:49 -0700

    custom middleware done

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,77 @@
-const express = require('express');
-
+const express = require("express");
+const postDb = require("./postDb");
+const logger = require("../middleware/logger");
+const validatePost = require("../middleware/validatePost");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get("/", logger(), (req, res) => {
+  postDb
+    .get(req.query)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Error getting posts" });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+// router.post("/:id", validatePost(), (req, res) => {
+//   users
+//     .insert(req.body)
+//     .then((user) => res.status(200).json(user))
+//     .catch((err) => res.status(500).json({ message: err }));
+// });
+
+router.get("/:id", logger(), validatePost(), (req, res) => {
+  postDb
+    .getById(req.params.id)
+    .then((post) => {
+      res.status(200).json(post);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Error getting post" });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete("/:id", logger(), validatePost(), (req, res) => {
+  postDb.getById(req.params.id).then((post) => {
+    res.status(200).json(post);
+  });
+  postDb.remove(req.params.id).catch((error) => {
+    console.log(error);
+    res.status(500).json({
+      error: "The post could not be removed",
+    });
+  });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put("/:id", logger(), validatePost, (req, res) => {
+  postDb
+    .update(req.params.id, req.params.body)
+    .then((post) => {
+      res.status(200).json(post);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: "The post information could not be modified.",
+      });
+    });
 });
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-  // do your magic!
-}
+// function validatePostId(req, res, next) {
+//   posts.getById(req.params.id).then((post) => {
+//     if (!post) {
+//       res.status(404).json({ message: "invalid post id" });
+//     } else {
+//       req.post = post;
+//       next();
+//     }
+//   });
+// }
 
 module.exports = router;
